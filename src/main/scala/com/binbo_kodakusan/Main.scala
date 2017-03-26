eda6376894492e54622d5c7c873c57b0243d1996
marony <marony@binbo-kodakusan.com>
2017-03-26 16:30:42 +0900

    first commit

---
+++ src/main/scala/com/binbo_kodakusan/Main.scala
@@ -0,0 +1,43 @@
+// https://wandbox.org/permlink/jqNHjKRonyRxYeQk
+// https://wandbox.org/permlink/UKaaA4A9GS8aaJM8
+
+sealed trait Nat {
+  def fold[T](f: T => T)(a: T): T
+  def toInt: Int
+
+  def plus(rhs: Nat): Nat =
+    this match {
+      case Succ(n) => Succ(n.plus(rhs))
+      case Zero() =>
+        rhs match {
+          case Succ(n) => rhs.plus(Zero())
+          case Zero() => rhs
+        }
+    }
+}
+
+object Nat {
+  def fromInt(n: Int): Nat = {
+    if (n <= 0)
+      Zero()
+    else
+      Succ(fromInt(n - 1))
+  }
+}
+
+case class Succ(pre: Nat) extends Nat {
+  def fold[T](f: T => T)(a: T): T = f(pre.fold(f)(a))
+  def toInt: Int = fold[Int](_ + 1)(0)
+}
+
+case class Zero() extends Nat {
+  def fold[T](f: T => T)(a: T): T = a
+  def toInt: Int = 0
+}
+
+object Main extends App {
+  val two = Nat.fromInt(2)
+  val three = Nat.fromInt(3)
+  println(s"${two.toInt} = $two, ${three.toInt} = $three")
+  println(s"${two.toInt} + ${three.toInt} = ${two.plus(three).toInt}")
+}
