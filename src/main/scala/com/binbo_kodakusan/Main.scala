a12a5d97ce1aef3e2bafe111480fe3bcda951cf2
Atsushi Tada <tada@tworks.co.jp>
2017-03-27 17:35:37 +0900

    simplify nested pattern match in plus method

--- src/main/scala/com/binbo_kodakusan/Main.scala
+++ src/main/scala/com/binbo_kodakusan/Main.scala
@@ -1,25 +1,23 @@
 // https://wandbox.org/permlink/jqNHjKRonyRxYeQk
 // https://wandbox.org/permlink/UKaaA4A9GS8aaJM8
 // https://wandbox.org/permlink/rs4yrRiDnXyshbxm
+// https://wandbox.org/permlink/C2xj50LYVVMgH0rM
 
 sealed trait Nat {
   def fold[T](f: T => T)(a: T): T
   def toInt: Int
 
   def plus(rhs: Nat): Nat =
-    this match {
-      case Succ(n) => Succ(n.plus(rhs))
-      case Zero() =>
-        rhs match {
-          case Succ(n) => rhs.plus(Zero())
-          case Zero() => rhs
-        }
+    (this, rhs) match {
+      case (Succ(n), _) => Succ(n.plus(rhs))
+      case (_, Succ(n)) => rhs.plus(Zero())
+      case _ => rhs
     }
-  
+
   def minus(rhs: Nat): Nat =
     (this, rhs) match {
-       case (Succ(m), Succ(n)) => m.minus(n)
-       case _ => this
+      case (Succ(m), Succ(n)) => m.minus(n)
+      case _ => this
     }
 }
 
@@ -46,6 +44,6 @@ object Main extends App {
   val two = Nat.fromInt(2)
   val three = Nat.fromInt(3)
   println(s"${two.toInt} = $two, ${three.toInt} = $three")
-  println(s"${two.toInt} + ${three.toInt} = ${two.plus(three).toInt}")
-  println(s"${three.toInt} + ${two.toInt} = ${three.minus(two).toInt}")
+  println(s"${two.toInt} + ${three.toInt} = ${(two plus three).toInt}")
+  println(s"${three.toInt} - ${two.toInt} = ${(three minus two).toInt}")
 }
